709db8615f615aac25714969ebcbcf3aaa28e5f2
Amcu <yudangblu@gmail.com>
2025-05-02 08:42:25 +0700

    updated simultaneously

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.node_modules.cofeshop {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
+    requires javafx.graphics;
+    
     opens com.node_modules.cofeshop to javafx.fxml;
     exports com.node_modules.cofeshop;
 }
